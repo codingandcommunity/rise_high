1c1e9d6cba0aeb4758bef35d5a314ce866f9e02b
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-08-12 16:23:23 -0700

    Add files via upload

---
+++ python/basic-python/input_ex_1.py
@@ -0,0 +1,5 @@
+name = input("Enter your name -> ")                     # input name
+age = input("Enter your age -> ")                       # input age
+fav_color = input("Enter your favorite color -> ")      # input favorite color
+
+print("Your name is", name + ", you are", age, "years old, and your favorite color is", fav_color+".")  # prints string with inputs
