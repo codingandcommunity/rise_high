9cc686166c6933b933575cbe5354dc4a3dfdb12c
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-08-19 19:51:56 -0400

    Add files via upload

---
+++ python/basic-python/guided_exercises/ch2_lists/lists.py
@@ -0,0 +1,8 @@
+a = ['cat', 'dog', 'bird', 'horse']
+b = [1, 4, 2, 3.4]
+
+# How do we access the 2nd element in b?
+
+# How do we delete the element 'bird' in a?
+
+# How do we add an element 'cow' to a?
