1dc58c0b988fe7ece1c60ad53ba836fc2341e41d
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-08-19 19:50:26 -0400

    Add files via upload

---
+++ python/basic-python/guided_exercises/ch1_variables/var_and_data.py
@@ -0,0 +1,5 @@
+# define variable 'name' and set it equal to your name
+
+# print the message "Hi, my name is" and your name to the terminal
+
+# get the type of the variable 'name' and print it to the terminal
