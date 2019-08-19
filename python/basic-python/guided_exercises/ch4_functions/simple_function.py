99cf36279cb4ec21e502520c48598211d1fd57ed
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-08-19 19:55:39 -0400

    Add files via upload

---
+++ python/basic-python/guided_exercises/ch4_functions/simple_function.py
@@ -0,0 +1,7 @@
+'''write a simple function to add two integers, 
+demonstrate how to call the function with various 
+inputs'''
+
+# call the function add, 2 and 3, print the result to the terminal
+
+# call the function add, 2 and 3, save the result as a variable
