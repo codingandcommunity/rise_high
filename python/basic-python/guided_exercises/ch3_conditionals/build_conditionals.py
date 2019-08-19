d85ed279d43e5d33df48107dad139efd85ddb9c1
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-08-19 19:53:11 -0400

    Add files via upload

---
+++ python/basic-python/guided_exercises/ch3_conditionals/build_conditionals.py
@@ -0,0 +1,11 @@
+# Let's build a conditional!
+
+# step 1: defining a boolean
+
+# step 2: boolean operators
+
+# OR
+
+# AND
+
+# NOT
---
+++ python/basic-python/guided_exercises/ch3_conditionals/build_if_elif_else.py
@@ -0,0 +1,1 @@
+# start by bulding a simple if-else statement, then add an elif
