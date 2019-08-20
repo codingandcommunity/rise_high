04eb76352ca42ab113743dcff03c640a106a2718
Stephanie Davis <44389609+stephanie-davis@users.noreply.github.com>
2019-08-19 17:11:39 -0700

    Create square.py

---
+++ turtle/guided_exercises/square.py
@@ -0,0 +1,15 @@
+# set up environment
+import turtle                    # import turtle library  
+
+window = turtle.Screen()      # create graphics window
+window.bgcolor("light blue")        # set background color
+
+pen = turtle.Turtle()         # create new Turtle object
+pen.shape("turtle")           # give the object a turtle shape
+
+# draw a square: manual
+
+# draw a square: with a loop
+
+# keep window up
+turtle.done()
